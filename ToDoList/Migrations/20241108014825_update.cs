5c3c264b96a2559f26a682e9e6bf587021f59703
fox-bones <mrcaysizzle@gmail.com>
2024-11-07 20:59:29 -0500

    first commit

---
+++ ToDoList/Migrations/20241108014825_update.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ch10ToDoList.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Statuses_StatusId",
+                table: "ToDos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusId",
+                table: "ToDos",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Statuses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Statuses_StatusId",
+                table: "ToDos",
+                column: "StatusId",
+                principalTable: "Statuses",
+                principalColumn: "StatusId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Statuses_StatusId",
+                table: "ToDos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusId",
+                table: "ToDos",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Statuses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Statuses_StatusId",
+                table: "ToDos",
+                column: "StatusId",
+                principalTable: "Statuses",
+                principalColumn: "StatusId");
+        }
+    }
+}
---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description.")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a due date.")]
+        public DateTime? DueDate { get; set; }
+
+        [Required(ErrorMessage = "Please select a category.")]
+        public string CategoryId { get; set; } = string.Empty;
+        public Category Category { get; set; } = new Category();
+
+        [Required(ErrorMessage = "Please select a status.")]
+        public string StatusId { get; set; } = string.Empty;
+        public Status Status { get; set; } = new Status();
+
+        public bool Overdue =>
+            StatusId == "open" && DueDate < DateTime.Today;
+    }
+}
